95fe4d0e38ea2f0430eabfb730e17c6a0e964f9e
Steve Commer <steve.commer@laposte.net>
2023-07-24 11:17:17 +0200

    creation mediscreen-ui

---
+++ mediscreen-ui/src/main/java/com/mediscreen/mediscreenui/MediscreenUiApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.mediscreenui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediscreenUiApplication.class, args);
+    }
+
+}
---
+++ mediscreen-ui/src/test/java/com/mediscreen/mediscreenui/MediscreenUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.mediscreenui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
