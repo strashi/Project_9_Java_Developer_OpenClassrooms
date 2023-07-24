36abd597e18d39f1b09323f07da63fc7a488c83a
Steve Commer <steve.commer@laposte.net>
2023-07-24 11:13:35 +0200

    creation data-patients-api

---
+++ data-patients-api/src/main/java/com/mediscreen/datapatientsapi/DataPatientsApiApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.datapatientsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPatientsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataPatientsApiApplication.class, args);
+    }
+
+}
