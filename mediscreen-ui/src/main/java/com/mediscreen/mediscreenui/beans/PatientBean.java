9ca1c574d1caf8fc3c381624968e8d7a6872160d
Steve Commer <steve.commer@laposte.net>
2023-08-22 13:43:40 +0200

    all fields validation

--- mediscreen-ui/src/main/java/com/mediscreen/mediscreenui/beans/PatientBean.java
+++ mediscreen-ui/src/main/java/com/mediscreen/mediscreenui/beans/PatientBean.java
@@ -2,10 +2,15 @@ package com.mediscreen.mediscreenui.beans;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import java.io.Serializable;
-import java.sql.Date;
+//import java.sql.Date;
+import java.util.Date;
 @Data
 public class PatientBean implements Serializable {
     private Long patientId;
@@ -13,10 +18,15 @@ public class PatientBean implements Serializable {
     private String firstName;
     @NotBlank(message = "LastName is mandatory")
     private String lastName;
-
-    @JsonFormat(pattern ="yyyy/MM/dd")
+    //@JsonFormat(pattern ="yyyy/MM/dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @NotNull(message = "dob is mandatory")
+    @Past(message = "dob in the past")
     private Date dob;
+    @NotBlank(message = "Sex is mandatory")
     private String sex;
+    @NotBlank(message = "Address is mandatory")
     private String address;
+    @NotBlank(message = "Phone is mandatory")
     private String phone;
 }
