fd66f0bda3ca647e8b95e1f1d364fefc5beb261c
Steve Commer <steve.commer@laposte.net>
2023-08-25 12:23:42 +0200

    swagger implementation

--- data-patients-api/src/main/java/com/mediscreen/datapatientsapi/DataPatientsApiApplication.java
+++ data-patients-api/src/main/java/com/mediscreen/datapatientsapi/DataPatientsApiApplication.java
@@ -1,8 +1,8 @@
 package com.mediscreen.datapatientsapi;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DataPatientsApiApplication {
 
