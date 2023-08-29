9af8a23148d0df03de72701ee06f62ff413c644a
Steve Commer <steve.commer@laposte.net>
2023-08-29 08:25:54 +0200

    practitioners note api implementation

---
+++ practitioners-notes-api/src/test/java/com/mediscreen/practitionersnotesapi/PractitionersNotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.practitionersnotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractitionersNotesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
