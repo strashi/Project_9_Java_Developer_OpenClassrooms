1b4399f8042b844585506a939f1c6bdcbc782def
Steve Commer <steve.commer@laposte.net>
2023-08-31 07:56:51 +0200

    structure for diabetes-detector-api

---
+++ diabetes-detector-api/src/main/java/com/mediscreen/diabetesdetectorapi/DiabetesDetectorApiApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.diabetesdetectorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetesDetectorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiabetesDetectorApiApplication.class, args);
+    }
+
+}
---
+++ diabetes-detector-api/src/test/java/com/mediscreen/diabetesdetectorapi/DiabetesDetectorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.diabetesdetectorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesDetectorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
